d7ee4755fd9662973ccefb91279076dc2ad3a2bc
Chad DeRosier <chad.derosier@gmail.com>
2020-02-08 14:44:49 -0800

    Image reloading and cycing points

--- brilliantimagery_ui/gui.py
+++ brilliantimagery_ui/gui.py
@@ -1,12 +1,13 @@
 import sys
 import time
+from copy import deepcopy
 from pathlib import Path
 
 from brilliantimagery.sequence import Sequence
 import numpy as np
 from PySide2 import QtGui
-from PySide2.QtCore import QFile, QSettings, QSize, Qt
-from PySide2.QtGui import QImage, QMouseEvent
+from PySide2.QtCore import QFile, QSettings, QSize, Qt, QPoint
+from PySide2.QtGui import QImage, QMouseEvent, QColor
 from PySide2.QtWidgets import QApplication, QMainWindow, QFileDialog
 
 from brilliantimagery_ui.gui_mainwindow import Ui_MainWindow
@@ -36,11 +37,12 @@ class MainWindow(QMainWindow):
 
     def open_sequence(self):
         folder = self.open_folder(self.ui.ramp_folder_edit,
+                                  # 'E:\\Pictures\\Album\\Timelapse\\test\\dng')
                                   self.default_values.value(self.default_source_folder_name))
         self.load_sequence(folder)
 
     def load_sequence(self, folder):
-        if not Path(folder).is_dir():
+        if not folder or not Path(folder).is_dir():
             return
         if not self.sequence or folder != self.sequence.path:
             self.sequence = Sequence(folder)
@@ -48,18 +50,45 @@ class MainWindow(QMainWindow):
 
         image = self.sequence.get_reference_image(index_order='yxc').astype(np.uint8)
         h, w, _ = image.shape
-        image = np.reshape(image, (image.size, ))
-        self.image = QImage(image, w, h, QImage.Format_RGB888)
+        self.image_data = np.reshape(image, (image.size, ))
+        self.image = QImage(self.image_data, w, h, QImage.Format_RGB888)
 
         self.draw_image()
 
-    def mousePressEvent(self, QMouseEvent):
-        # print mouse position
-        print(QMouseEvent.pos())
+    def mouseReleaseEvent(self, event: QMouseEvent):
+        if not self.image:
+            return
+
+        image_location = self.ui.ramp_image.mapToGlobal(QPoint(0, 0))
+        image_x = image_location.x()
+        image_y = image_location.y()
+        image_width = self.image.width()
+        image_height = self.image.height()
+        x = event.globalX()
+        y = event.globalY()
+
+        if not (image_x <= x <= image_x + image_width and image_y <= y <= image_y + image_height):
+            return
+
+        if not self.point1:
+            self.point1 = (x, y)
+        elif not self.point2:
+            self.point2 = (x, y)
+        else:
+            self.point1 = ()
+            self.point2 = ()
+
+        self.draw_image()
 
     def draw_image(self):
         canvas = QtGui.QPixmap(self.image)
         self.ui.ramp_image.setPixmap(canvas)
+
+        if self.point2:
+            painter = QtGui.QPainter(self.ui.ramp_image.pixmap())
+            painter.setPen(QColor(255, 0, 0))
+            painter.drawLine(10, 10, 50, 50)
+            painter.end()
 
     def open_folder(self, line_edit, start_location):
         folder = QFileDialog.getExistingDirectory(self, "Open Directory",
